5ede04b16893ea8ae36aa9b9d6f9ce30fe304651
MattSun04 <sunyutian@hotmail.com>
2016-12-02 22:28:29 +0800

    Add files via upload

---
+++ shingle-mr/mapper.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import jieba
+import sys
+import os
+#from gensim import corpora, models, similarities
+
+#jieba.load_userdict('dict.txt')
+##extract all possible words
+
+
+def mode_precise(k,f):
+    shin_list=[]
+    #f= open('dat','r')
+    raw=f.read()
+    word_list = list(jieba.cut(raw, cut_all = False, HMM=False))
+    stopw = set([line.strip() for line in open('stopwords.txt').readlines()])
+    ## delete the stopword and append k nearest one
+
+    offset=0
+    for i in range(len(word_list)):
+        if word_list[i-offset] in stopw or word_list[i-offset]==' ':
+            del word_list[i-offset]
+            offset+=1
+
+    for i in range(len(word_list)-k):
+        string=""
+        for j in range(k):
+            string+=word_list[i+j]
+        shin_list.append(string)
+    #print(word_list)
+    for j in shin_list:
+	j=j.encode('UTF-8')       
+	print '%s\t%s' % (j, os.environ['mapreduce_map_input_file'].split('/')[-1])
+
+f=sys.stdin
+mode_precise(2,f)
+#raw=f.read()
+#raw=raw.replace('\n',' ')
+#word_list = jieba.cut(raw, cut_all = True, HMM=False)
+#stopw = [line.strip() for line in open('stopwords.txt').readlines()]
+
+#word_list=set(word_list)-set(stopw)
+#for i in word_list:
+#	i=i.encode('UTF-8')       
+#	print '%s\t%s' % (i, os.environ['mapreduce_map_input_file'].split('/')[-1])
+#mode_all(f)
+#mode_precise(2,f)
---
+++ shingle-mr/reducer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+
+
+file=sys.stdin
+
+dic=[]
+a=[]
+for i in file:
+    d,t=i.split('\t')
+    t=t.strip()
+    if d in dic:
+	if t in a[dic.index(d)]:
+		continue
+	else:
+        	a[dic.index(d)].append(t)
+    else:
+        dic.append(d)
+        l=[t]
+        a.append(l)
+
+
+for i in range(len(dic)):
+    print dic[i],a[i]
+        
+
+
